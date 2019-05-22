a27e88104f13d39d8495864661995a8ec3e27d99
Trung Bùi <bqtrung@tma.com.vn>
2019-05-22 09:39:41 +0700

    Functional Interface

---
+++ src/main/java/bqtrung/demospring/repository/impl/FuntionalInterfaceTest1.java
@@ -0,0 +1,12 @@
+package bqtrung.demospring.repository.impl;
+
+@FunctionalInterface
+public interface FuntionalInterfaceTest1 {
+	// Functional Interface only one Abstract method and must be one method :)
+	public void sayHello(String helloMessage);
+	
+	// Functional Interface can contain many method of java.lang.Object class and scope must be public
+    int hashCode(); 
+    String toString();
+    boolean equals(Object obj);
+}
---
+++ src/main/java/bqtrung/demospring/repository/impl/FuntionalInterfaceTest2.java
@@ -0,0 +1,6 @@
+package bqtrung.demospring.repository.impl;
+
+@FunctionalInterface
+public interface FuntionalInterfaceTest2 extends FuntionalInterfaceTest, FuntionalInterfaceTest1  {
+	//Một Functional Interface có thể mở rộng một Interface khác chỉ khi nó không có bất kỳ phương thức trừu tượng nào
+}
